68e2f8b243856cc3cddc3b86b8495f02ffa4d65e
Piotr Jackowski <pjackowski18@gmail.com>
2021-04-19 00:52:17 -0500

    updated route for sharing calender for signup

--- src/server/routes.py
+++ src/server/routes.py
@@ -1,10 +1,12 @@
 import os
 import secrets
+import json
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import engine
 from flask import render_template, url_for, flash, redirect, request, jsonify
 from flask.globals import session
 from server import app #db, bcrypt
+
 from server.models import *
 
 
@@ -22,4 +24,12 @@ def login():
 
 @app.route("/calendar")
 def calendar():
-    return render_template('calendar.html')
+    #need to pass in user
+    user = {"admin":"true", "username":"test"}
+
+    return render_template('calendar.html', user = user)
+
+@app.route("/calendar/<username>")
+def appointmentSignUp(username):
+    #get user from db
+    return render_template('appointmentSignUp.html', admin="false", user= username);
