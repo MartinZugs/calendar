ebdf38c6bd570bd4d11fddfe909a6026512ea80d
Martin <martin.e.zugschwert@gmail.com>
2021-04-27 21:36:18 -0500

    login and register changes

--- src/server/routes.py
+++ src/server/routes.py
@@ -4,8 +4,10 @@ import json
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import engine
 from flask import render_template, url_for, flash, redirect, request, jsonify
+from server.forms import RegistrationForm, LoginForm
 from flask.globals import session
-from server import app #db, bcrypt
+from flask_login import login_user, current_user, logout_user, login_required
+from server import app, db, bcrypt
 
 from server.models import *
 
@@ -20,9 +22,35 @@ connection = db.engine.connect()
 def home():
     return render_template('home.html', title='Home')
 
-@app.route("/login")
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    
+    if current_user.is_authenticated:
+        flash('You are already logged in!', 'danger')
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('calendar.html', title='Login', form=form)
 
 @app.route("/calendar")
 def calendar():
